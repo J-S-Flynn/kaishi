2b22d25067e48e09108d6758da250e29c52d27ad
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2020-07-26 16:33:06 +0100

    init commit

---
+++ cmake-build-debug/CMakeFiles/kaishi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaishi.dir/main.cpp.o"
+  "kaishi"
+  "kaishi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaishi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
